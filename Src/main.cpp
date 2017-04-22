c89f880351e2406dc8a538ff2646d10a21aedabd
Yikealo Abraha <yekalob@gmail.com>
2017-04-22 16:11:19 -0700

    Cleared mistakenly included files
    Cleared mistakenly included files and rearranged it so that all source
    files are located in SRC folder

--- Src/main.cpp
+++ Src/main.cpp
@@ -2,53 +2,14 @@
 //  main.cpp
 //  teensyMicromouse
 //
-//  Created by Yikealo Abraha  on 4/13/17.
+//  Created by Yikealo Abraha  on 4/22/17.
 //  Copyright © 2017 Yikealo Abraha . All rights reserved.
 //
 
 #include <stdio.h>
-#include <iostream>
-#include <time.h>
-
-
-using namespace std;
 
 
 int main(){
-    
-   
-    clock_t t = clock();
-    
-   
-    
-    for(int x=0;x<19;++x){
-        
-        std::cout << "----" ;;
-        
-    }
-    cout<<endl;
-    
-    for(int m=0;m<16;++m){
-        
- 
-    for(int x=0;x<16;++x){
-    
-        std::cout << "|    " ;
-        
-    }
-     
-    for(int x=0;x<19;++x){
-        
-        std::cout << "----" ;;
-        
-    }
-        cout <<endl;
-        
-        
-    }
-  
-    
-    cout<<clock()-t<<endl;
     
     return 0;
 }
